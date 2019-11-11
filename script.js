412c58e9dcc4e4c8e921eb0077e838649129886c
jkinsinger <52002561+jkinsinger@users.noreply.github.com>
2019-11-11 09:30:59 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    $('.social2').hover(
+        function () {
+            $(this).css({"width": "100px"});
+        },
+        function () {
+            $(this).css({"width": "50px"});
+        });
+
+    $('.social3').hover(
+        function () {
+            $(this).css({"width": "100px"});
+        },
+        function () {
+            $(this).css({"width": "50px"});
+        });
+    
+    $('.social4').hover(
+        function () {
+            $(this).css({"width": "200px"});
+        },  
+        function () {
+           $(this).css({"width": "100px"});
+        });
+
+    $('.social5').hover(
+        function () {
+            $(this).css({"width": "100px"});
+        },
+        function () {
+            $(this).css({"width": "50px"});
+        });
+});
